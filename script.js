150a20407936479e8e958ba2e3fb3439e379ab8f
Hithishini <127782094+Hithishini@users.noreply.github.com>
2023-07-26 00:54:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("ul");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  //span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+
 
