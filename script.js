4219d975112445ee7ebd79773cf120ae3361c137
Hithishini <127782094+Hithishini@users.noreply.github.com>
2023-07-26 15:33:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
+const inputBox=document.getElementById("task");
+const listContainer=document.getElementById("Task_list");
 
+function newElement()
+ {
+     let li=document.createElement("li");
+     li.innerHTML=inputBox.value;
+     listContainer.appendChild(li);
+     inputBox.value=" ";
+
+     let del=document.createElement("button");
+     del.innerHTML="Delete";
+     li.appendChild(del);   
+
+ }
+
+
+listContainer.addEventListener("click",function(e)
+{
+    if(e.target.tagName==="LI")
+    {
+        e.target.classList.toggle("done");
+    }
+    else if (e.target.tagName ==="BUTTON")
+    {
+        e.target.parentElement.remove();
+    }
+},false);
+
+
+
+
+
+
+
+
+
+   
 
